b3797ac89abb927c22e279a15a499418d7cbf07b
Devin <devdhera@gmail.com>
2020-07-07 11:40:46 +0530

    Refactored class route to use controllers

--- routes/classes.js
+++ routes/classes.js
@@ -1,24 +1,10 @@
 const express = require('express');
+const { getClasses, getClass, createClass, updateClass, deleteClass } = require('../controllers/classes');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all classes' });
-});
+router.route('/').get(getClasses).post(createClass);
 
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show class ${req.params.id}` });
-});
-
-router.post('/', (req, res) => {
-    res.status(201).json({ success: true, msg: 'Create new class' });
-});
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update class ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete class ${req.params.id}` });
-});
+router.route('/:id').get(getClass).put(updateClass).delete(deleteClass);
 
 module.exports = router;
